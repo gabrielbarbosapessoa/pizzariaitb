19c1ad86fb2916f1d1eaf0b25c15dc74568c064e
gabrielbarbosapessoa <gabrielbarbosapessoa2@gmail.com>
2024-08-01 09:01:37 -0300

    codigo final

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
+import {useState} from 'react'
 import './App.css'
-import { Routes, Route} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
+import Listagem from './pages/Listagem';
 
 const App = () => {
 
+  const [color] = useState("#0000FF")
+  document.body.style.backgroundColor = color
+
   return (
     <Routes>
-      <Route path="/" />
+      <Route path="/" element={<Listagem />}/>
     </Routes>
   )
 }
-
 
 export default App;
